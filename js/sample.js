294153f1cfe44f865095542fe277ea19905c9876
hansol <kwack1107@naver.com>
2021-06-17 02:14:49 +0900

    기존 작업 커밋

---
+++ js/sample.js
@@ -0,0 +1,9 @@
+/**
+ * webcontent>js폴더 만들고 > new> other> javascript>javascript source file 
+ * 
+ */
+
+function external(){
+		alert("별도로 작성된 js파일에서 external()함수 실행");
+		// window.alert 이나 alert이나 둘다 같음.
+}
